a621e9d23500f9c2b0c201f7e0922e026f5e1009
Tom Chambers <tom.chambers@gmail.com>
2016-01-06 12:32:06 +0000

    v1.1

--- undead.js
+++ undead.js
@@ -1,20 +1,24 @@
 $('th:contains("Died")').parent().hide()
-$('td:contains("death")').parent().hide()
 $('th:contains("death")').parent().hide()
+$('th:contains("Resting place")').parent().hide()
+
+$('td:contains("death")').parent().hide()
+
 $('span:contains("Death")').parent().hide()
+
 $('p').not('.infobox p').first().html(function() {
-	return $(this).html().replace('was', 'is');
+	return $(this).html().replace('was', 'is')
 })
 
-var hidden = ['died','remains','funeral','death','autopsy','suicide','cremated']
+var hidden = ['died','remains','funeral','death','autopsy','suicide','cremated','interred','mausoleum']
 hidden.forEach(function(word) {
-	$('p:contains('+word+')').hide();
-	$('li:contains('+word+')').hide();
-	$('span:contains('+word+')').hide();	
+	$('p:contains('+word+')').hide()
+	$('li:contains('+word+')').hide()
+	$('span:contains('+word+')').hide()	
 })
 
 $('p').each(function() {
-    var html = $(this).html();
-    $(this).html(html.replace('was survived by', 'lives with'));
-    $(this).html(html.replace(/(&nbsp;| )– [0-9a-zA-Z ]+\)/, ')'));
-});
+    var html = $(this).html()
+    $(this).html(html.replace('was survived by', 'lives with'))
+    $(this).html(html.replace(/(&nbsp;| )– [0-9a-zA-Z, ]+\)/, ')'))
+})
